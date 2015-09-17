7d747ed26c0a6ea31b300d5bfc2125c73bac845d
bluezebragames <bluezebragames@gmail.com>
2015-09-17 17:58:55 -0600

    First commit!  We've got a stolen Nullpomino background and that's about it

---
+++ main.js
@@ -0,0 +1,8 @@
+var main = function () {
+    "use strict";
+    var canvas = document.getElementById("canvas"), context = canvas.getContext("2d");
+    context.drawImage(document.getElementById("background"), 0, 0);
+    /*context.fillStyle = "#404040";
+    context.fillRect(0, 0, 30, 30);*/
+};
+

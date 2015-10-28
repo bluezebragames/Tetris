d5a3acb7eec247c8c38ef49d816172fd1e514e3e
bluezebragames <bluezebragames@gmail.com>
2015-10-28 09:33:41 -0600

    Rotation, line clearing, multiple piece spawning, improved key handling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // declare variables up here because jslint doesn't like me
-var loop, counter = 0, canvas, context, now, array, x, y;
+var loop, counter = 0, canvas, context, now, array, x, y, piece, topleftx, toplefty, rotposn, rotArray, keys = [];
 
 // start everything up!
 var main = function () {
@@ -26,6 +26,156 @@ var main = function () {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    rotArray = [[[0, 0, 0, 0],
+                 [1, 1, 1, 1],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 0]],
+               
+                [[0, 0, 0, 0],
+                 [1, 1, 1, 1],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 0]],
+               
+               
+                [[1, 1, 1, 0],
+                 [1, 0, 0, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 0, 0],
+                 [0, 0, 1, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0]],
+                
+                
+                [[0, 1, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+                
+               
+                [[1, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[1, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 1, 0],
+                 [0, 1, 1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+                
+                
+                [[0, 0, 0, 0],
+                 [1, 1, 1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+                           
+               
+                [[1, 1, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 0, 0, 0],
+                 [1, 0, 0, 0],
+                 [1, 1, 1, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 1, 0],
+                 [0, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+                
+               
+                [[0, 1, 1, 0],
+                 [1, 1, 0, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[1, 0, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[0, 1, 1, 0],
+                 [1, 1, 0, 0],
+                 [0, 0, 0, 0],
+                 [0, 0, 0, 0]],
+               
+                [[1, 0, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]]];
+    
+    topleftx = 3;
+    toplefty = -1;
+    rotposn = 0;
+    piece = 8;
     
     
     loop();
@@ -84,7 +234,7 @@ var moveDown = function () {
     "use strict";
     for (y = 19; y >= 0; y -= 1) {
         for (x = 0; x < 10; x += 1) {
-            if (array[y][x] >= 7) {
+            if (array[y][x] > 7) {
                 if (y === 19) {
                     return;
                 }
@@ -93,6 +243,7 @@ var moveDown = function () {
             }
         }
     }
+    toplefty += 1;
 };
 
 
@@ -100,7 +251,7 @@ var moveLeft = function () {
     "use strict";
     for (x = 0; x < 10; x += 1) {
         for (y = 0; y < 20; y += 1) {
-            if (array[y][x] >= 7) {
+            if (array[y][x] > 7) {
                 if (x === 0) {
                     return;
                 }
@@ -109,6 +260,7 @@ var moveLeft = function () {
             }
         }
     }
+    topleftx -= 1;
 };
 
 
@@ -116,7 +268,7 @@ var moveRight = function () {
     "use strict";
     for (x = 9; x >= 0; x -= 1) {
         for (y = 0; y < 20; y += 1) {
-            if (array[y][x] >= 7) {
+            if (array[y][x] > 7) {
                 if (x === 9) {
                     return;
                 }
@@ -125,7 +277,224 @@ var moveRight = function () {
             }
         }
     }
+    topleftx += 1;
 };
+
+////////
+//ROTATE
+////////
+
+var canRotate = function () {
+    "use strict";
+    for (x = topleftx; x < topleftx + 4; x += 1) {
+        for (y = toplefty; y < toplefty + 4; y += 1) {
+            if (array[y][x] !== 0 && array[y][x] < 8) {
+                if (rotArray[rotposn + 4 * (piece - 8)][y - toplefty][x - topleftx]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+
+var rotate = function (dir) {
+    "use strict";
+    
+    if (!canRotate()) {
+        return;
+    }
+    
+    rotposn = (rotposn + 1) % 4;
+    
+    // clear out old piece
+    for (x = topleftx; x < topleftx + 4; x += 1) {
+        for (y = toplefty; y < toplefty + 4; y += 1) {
+            if (array[y][x] > 7) {
+                array[y][x] = 0;
+            }
+        }
+    }
+    
+    // add new piece
+    for (x = topleftx; x < topleftx + 4; x += 1) {
+        for (y = toplefty; y < toplefty + 4; y += 1) {
+            if (rotArray[rotposn + 4 * (piece - 8)][y - toplefty][x - topleftx]) {
+                array[y][x] = piece;
+            }
+        }
+    }
+};
+
+////////////////
+//CLEARING LINES
+////////////////
+
+var clearLines = function () {
+    "use strict";
+    var clear, numlines = 0;
+    for (y = 19; y >= 0; y -= 1) {
+        clear = true;
+        for (x = 0; x < 10; x += 1) {
+            if (array[y][x] === 0 || array[y][x] > 7) {
+                clear = false;
+            }
+        }
+        if (clear) {
+            numlines += 1;
+            for (x = 0; x < 10; x += 1) {
+                array[y][x] = 0;
+            }
+        } else {
+            for (x = 0; x < 10; x += 1) {
+                if (array[y][x] < 8) {
+                    array[y + numlines][x] = array[y][x];
+                }
+            }
+        }
+    }
+};
+
+
+//////
+//LOCK
+//////
+
+var lock = function () {
+    "use strict";
+    for (x = 0; x < 10; x += 1) {
+        for (y = 0; y < 20; y += 1) {
+            if (array[y][x] > 7) {
+                array[y][x] -= 7;
+            }
+        }
+    }
+};
+
+///////////
+//NEW PIECE
+///////////
+
+var newPiece = function () {
+    "use strict";
+    lock();
+    
+    piece = Math.floor(7 * Math.random()) + 8;
+    rotposn = 0;
+    
+    switch (piece) {//iloztjs
+    case 8:
+        array[0][3] = piece;
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[0][6] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 9:
+        array[0][3] = piece;
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[1][3] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 10:
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[1][4] = piece;
+        array[1][5] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 11:
+        array[0][3] = piece;
+        array[0][4] = piece;
+        array[1][4] = piece;
+        array[1][5] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 12:
+        array[0][3] = piece;
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[1][4] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 13:
+        array[0][3] = piece;
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[1][5] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    case 14:
+        array[0][4] = piece;
+        array[0][5] = piece;
+        array[1][3] = piece;
+        array[1][4] = piece;
+        topleftx = 3;
+        toplefty = -1;
+        break;
+    }
+};
+
+var doesLock = function () {
+    "use strict";
+    for (x = 0; x < 10; x += 1) {
+        for (y = 0; y < 20; y += 1) {
+            if (array[y][x] > 7 && (y === 19 || (array[y + 1][x] > 0 && array[y + 1][x] <= 7))) {
+                newPiece();
+            }
+        }
+    }
+};
+
+///////
+//DEBUG
+///////
+
+var debugPrint = function () {
+    "use strict";
+    alert(toplefty);
+};
+
+/////////////
+//KEY HANDLER
+/////////////
+
+var handleKeys = function () {
+    "use strict";
+        
+    if (keys[81]) {
+        debugPrint();
+        keys[81] = false;
+    }
+    if (keys[37]) {
+        moveLeft();
+        keys[37] = false;
+    }
+    if (keys[39]) {
+        moveRight();
+        keys[39] = false;
+    }
+    if (keys[40]) {
+        moveDown();
+        keys[40] = false;
+    }
+    if (keys[90]) {
+        rotate(-1);
+        keys[90] = false;
+    }
+    if (keys[88]) {
+        rotate(-1);
+        keys[88] = false;
+    }
+};
+
 
 
 
@@ -133,6 +502,9 @@ var loop = function () {
     "use strict";
     
     draw();
+    doesLock();
+    clearLines();
+    handleKeys();
     
     
     /*context.fillStyle = "#404040";
@@ -142,23 +514,13 @@ var loop = function () {
 
 
 
-
 function getChar(event) {
     "use strict";
-    switch (event.keyCode) {
-    case 37:
-        moveLeft();
-        break;
-    case 39:
-        moveRight();
-        break;
-    case 40:
-        moveDown();
-        break;
-    }
+    keys[event.keyCode] = event.type === 'keydown';
 }
 
 window.onload = function () {
     "use strict";
     window.addEventListener("keydown", getChar, false);
+    window.addEventListener("keyup", getChar, false);
 };
